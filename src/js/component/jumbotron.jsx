690c83ef1e3225badacbe3527e9fc723d2fe59ad
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-10-29 22:00:18 +0000

    updated jumbotron

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Jumbotron = () => {
 	return (
-		<div className="container py-5 bg-light rounded">
+		<div className="container py-5 px-5 bg-light rounded">
             <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
             <p className="col fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni porro sint dolores sit, eveniet est dolorum illum numquam doloribus, impedit totam? Amet, vel reiciendis laboriosam error consequuntur quaerat delectus non voluptate quia aperiam, odio nesciunt, veritatis quo tempore assumenda sapiente.</p>
             <button className="btn btn-primary btn-lg" type="button">Call to action!</button>
